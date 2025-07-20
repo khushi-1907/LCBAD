10a6d1f67987ceba88e38fe5de65dd7daf0d89e7
Jashan Bansal <cec235008.cec@cgc.edu.in>
2025-07-20 22:35:14 +0530

    Update for GitHub Pages deployment

--- gamer-dream-comics-main/vite.config.ts
+++ gamer-dream-comics-main/vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Use subdirectory for GitHub Pages
-  base: "LCBAD", // e.g., /gamer-dream-comics/
+  base: "/LCBAD/", // e.g., /gamer-dream-comics/
   server: {
     host: "::",
     port: 8080,
