178e8198886bef932347f317bf399490f4d95411
Ananya9606 <srivastavaananya.956@gmail.com>
2025-08-07 18:54:41 +0530

    fix: clean up docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/LCBAD/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
